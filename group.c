c8bfaac683b91c26f7cd85a7b7a515a70da97449
robert-gee <robby.gee94@gmail.com>
2019-02-05 14:51:04 -0700

    Initial setup of group.c

---
+++ group.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void memberRob(){
+//member one info
+}
+
+void memberDavid(){
+//member2 info
+}
+
+void memberColton(){
+//member 3 info
+}
+
+void memberQihui(){
+//member 4 info
+}
+void memberBrendan(){
+//member 5 info
+}
+
+int main(){
+	printf("----------------Group-4----------------\n");
+	printf("---------------------------------------\n");
+	printf("----------------Members----------------\n");
+	
+	memberRob();
+	memberDavid();
+	memberColton();
+	memberQihui();
+	memberBrendan();
+
+	return 1;
+}	
